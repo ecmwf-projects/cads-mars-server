5f95a8617078234f56a0d832e66e1601d49e3f63
Gionata <gionata.biavati@ecmwf.int>
2025-04-05 10:44:48 +0000

    Fix syntax error in local_target function by correcting string quotes

--- cads_mars_server/config.py
+++ cads_mars_server/config.py
@@ -22,6 +22,6 @@ def local_target(cache_object: dict) -> str:
     if cache_object.get('target'):
         _c = get_config()
         target = cache_object['target']
-        _, _file = tuple(target.split(f'/{_c.get('cache_folder',"mars")}/'))
+        _, _file = tuple(target.split(f'/{_c.get("cache_folder","mars")}/'))
         _cache_root, share = _.split('/')[1:]
         return target.replace(f'/{_cache_root}/', f"{_c['CACHE_ROOT']}")
